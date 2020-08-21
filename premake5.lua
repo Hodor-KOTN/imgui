0b5cebfd09a2ff21602a0b715f761530bebb3be8
Ciprian Gavrilovici <ciprian.gavrilovici@gmail.com>
2020-08-21 21:58:43 +0300

    added premake project file excluded binaries & visual studio files from git

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+    
+	filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+	filter "configurations:Debug"
+		symbols "On"
+		
+	filter "configurations:Develop"
+		symbols "On"
+		
+	filter "configurations:Release"
+		optimize "On"
+		
+	filter "configurations:Deploy"
+		optimize "On"
